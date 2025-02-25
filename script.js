78878e3a48292c0aff646b099e8f1b312fe7c46b
StarSausage <Squindo@live.com>
2025-02-25 11:56:31 +0100

    Work

--- script.js
+++ script.js
@@ -1,81 +1,79 @@
 let ws = new WebSocket('wss://alchemist-interactive-5b89476a4dec.herokuapp.com/:443');
 
+//SUBTMIT MONTH
+let opacityValue = 0;
+let jsonObject = 
+{
+'fadeOut': opacityValue,
+'fadeIn': opacityValue,
+'month' : document.getElementById("week").value,
+'userID': userID
+};
+let fadedIn = false;
+let submit = document.querySelector('.Submitter');
+submit.addEventListener('mousedown', (event) => {
+    if(fadedIn==false){
+        fadeIn();
+    }
+    else{
+        fadeOut();
+    }
+        
+    ws.send(JSON.stringify({'month': document.getElementById("week").value,'radio1': domeSection}))
+});
+
+var button = document.getElementById('buttons');
+
+var buttons = button.querySelectorAll('input');
+
+for ( var i =0; i < buttons.length; i++){
+    buttons[i].addEventListener("mousedown", buttonClick);
+}
+
+function buttonClick(){
+    domeSection = this.value;
+    this.focus();
+    console.log(this, domeSection);
+}
+
+function fadeOut() {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.01){
+            clearInterval(timer);
+            op=0;
+        }
+        opacityValue = op;
+        opacityValue = op * 100;
+        op -= op * 0.01;
+        fadedIn = false;
+        console.log(opacityValue)
+        jsonObject = {'fadeOut': opacityValue, 'fadeIn': opacityValue};
+        ws.send(JSON.stringify(jsonObject))
+        console.log(domeSection);
+        
+    }, 10);
+}
+function fadeIn() {
+    var op = 0.1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op >= 10){
+            clearInterval(timer);
+            
+        }
+        opacityValue = op;
+        opacityValue = op * 10;
+        op += op * 0.01;
+        fadedIn = true;
+        console.log(opacityValue)
+        jsonObject = {'fadeOut': opacityValue, 'fadeIn': opacityValue};
+        ws.send(JSON.stringify(jsonObject))
+        console.log(domeSection);
+    }, 10);
+}
 
 //CONTROL TD
-let controlledByTD = document.querySelector('.controlledByTD');
 
-let controlTD1 = document.querySelector('.controllTD1');
-let controlTD = document.getElementById('1');
-controlTD1.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD1.value}))
-});
-
-let controlTD2 = document.querySelector('.controllTD2');
-controlTD2.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD2.value}))
-});
-
-let controlTD3 = document.querySelector('.controllTD3');
-controlTD3.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD3.value}))
-});
-
-let controlTD4 = document.querySelector('.controllTD4');
-controlTD4.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD4.value}))
-});
-
-let controlTD5 = document.querySelector('.controllTD5');
-controlTD5.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD5.value}))
-});
-
-let controlTD6 = document.querySelector('.controllTD6');
-controlTD6.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD6.value}))
-});
-
-let controlTD7 = document.querySelector('.controllTD7');
-controlTD7.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD7.value}))
-});
-let controlTD8 = document.querySelector('.controllTD8');
-controlTD8.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD8.value}))
-});
-
-
-let controlTD9 = document.querySelector('.controllTD9');
-controlTD9.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD9.value}))
-});
-
-let controlTD10 = document.querySelector('.controllTD10');
-controlTD10.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD10.value}))
-});
-
-let controlTD11 = document.querySelector('.controllTD11');
-controlTD11.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD11.value}))
-});
-
-let controlTD12 = document.querySelector('.controllTD12');
-controlTD12.addEventListener('mousedown', (event) => {
-    console.log('click!')
-    ws.send(JSON.stringify({'radio1': controlTD12.value}))
-});
 
 
 //WEBSOCKET
